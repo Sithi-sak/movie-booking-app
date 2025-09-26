fd58a4e3f8f0dfa431d69261cccbaa4fb4c70ad8
songKimvisal <songkimvisal21@gmail.com>
2025-09-26 15:54:17 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movie_booking_app/theme/app_theme.dart';
+import 'package:movie_booking_app/views/screen_control/screen_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text("Hello")
+      title: 'Movie App',
+      theme: AppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: const ScreenController(),
     );
   }
 }
-
